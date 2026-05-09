bbd35d8ee973121f0f0980c269c9720fd0aaba18
dybev <54762882+DyBev@users.noreply.github.com>
2026-05-09 22:42:07 +0100

    chore(comment): added comment about error handling

--- src/components/ErrorHandling.tsx
+++ src/components/ErrorHandling.tsx
@@ -14,11 +14,19 @@ export const ErrorHandling = ({
   error,
   children,
 }: ErrorProps) => {
+
+  /*
+    * If there are any location to report errors too this is where you would put it
+    * Creating a single component exclusively for managing any error state
+  */
+
   if (error) {
-    return <ErrorBox>
-      {error.status === 404 && I18N.PAYMENT_NOT_FOUND }
-      {error.status === 500 && I18N.INTERNAL_SERVER_ERROR }
-    </ErrorBox>
+    return (
+      <ErrorBox>
+        {error.status === 404 && I18N.PAYMENT_NOT_FOUND }
+        {error.status === 500 && I18N.INTERNAL_SERVER_ERROR }
+      </ErrorBox>
+    )
   }
 
   return children;
