95527fb21bdad7127917d7072412f9309ee12b74
DyBev <54762882+DyBev@users.noreply.github.com>
2026-05-09 10:24:46 +0100

    chore(types): added typing for the payment data

--- src/types/payment.ts
+++ src/types/payment.ts
@@ -1,4 +1,15 @@
+type Currency = "USD" | "EUR" | "GBP" | "AUD" | "CAD" | "ZAR" | "JPY" | "CZK";
+type Status = "completed" | "pending" | "failed" | "refunded";
+
 export interface Payment {
+    id: string,
+    customerName: string,
+    amount: number,
+    customerAddress: string,
+    currency: Currency,
+    status: Status,
+    date: string,
+    description: string,
 }
 
 export interface PaymentSearchResponse {
