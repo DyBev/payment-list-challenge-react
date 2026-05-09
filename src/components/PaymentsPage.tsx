14ac9ece95cb7a5c894ee8cf2232b1fa4d130663
dybev <54762882+DyBev@users.noreply.github.com>
2026-05-09 12:29:39 +0100

    chore(step1): added the step 1 solution

--- src/components/PaymentsPage.tsx
+++ src/components/PaymentsPage.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { Container } from './components'
+import { PaymentsTable } from "./PaymentsTable";
+import { PaymentsHeader } from "./PaymentsHeader";
 
 export const PaymentsPage = () => {
-  return <Container>Start here</Container>;
+  return (
+    <Container>
+      <PaymentsHeader />
+      <PaymentsTable />
+    </Container>
+  );
 };
---
+++ src/components/PaymentsTable.tsx
@@ -0,0 +1,57 @@
+import Recat from 'react';
+import { StatusBadge, Table, TableBodyWrapper, TableCell, TableHeader, TableRow, TableWrapper } from './components';
+import { I18N } from '../constants/i18n';
+import { usePayments } from '../hooks/usePayments';
+import { PaymentSearchResponse } from '../types/payment';
+
+export const PaymentsTable = () => {
+  const { data, isLoading } = usePayments({ 
+    paymentID: 'mockPaymentId'
+  })
+
+  if (isLoading) {
+    return "LOADING ..."
+  }
+
+  const {
+    payments,
+  } = data as PaymentSearchResponse;
+
+  return (
+    <TableWrapper>
+      <Table>
+        <thead>
+          <TableRow>
+            <TableHeader>{I18N.TABLE_HEADER_PAYMENT_ID}</TableHeader>
+            <TableHeader>{I18N.TABLE_HEADER_DATE}</TableHeader>
+            <TableHeader>{I18N.TABLE_HEADER_AMOUNT}</TableHeader>
+            <TableHeader>{I18N.TABLE_HEADER_CUSTOMER}</TableHeader>
+            <TableHeader>{I18N.TABLE_HEADER_CURRENCY}</TableHeader>
+            <TableHeader>{I18N.TABLE_HEADER_STATUS}</TableHeader>
+          </TableRow>
+        </thead>
+        <TableBodyWrapper>
+          {(payments).map(({
+            id,
+            customerName,
+            amount,
+            currency,
+            status,
+            date,
+          }) => {
+            return (
+              <TableRow key={id}>
+                <TableCell>{id}</TableCell>
+                <TableCell>{new Date(date).toLocaleString()}</TableCell>
+                <TableCell>{amount}</TableCell>
+                <TableCell>{customerName}</TableCell>
+                <TableCell>{currency}</TableCell>
+                <TableCell><StatusBadge status={status}>{status}</StatusBadge></TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBodyWrapper>
+      </Table>
+    </TableWrapper>
+  );
+};
